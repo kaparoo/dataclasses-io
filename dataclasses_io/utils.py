c9c68d714b4e0dd95dca2d5693baf53eb3e52173
kaparoo <kaparoo2001@gmail.com>
2022-01-20 17:55:42 +0900

    :adhesive_bandage: Fix load() to handle .yml file

--- dataclasses_io/utils.py
+++ dataclasses_io/utils.py
@@ -11,11 +11,20 @@ __all__ = ["_validate_path_format", "_validate_path_type"]
 _VALID_FORMATS = {"json", "yaml"}
 
 
+_SUFFIX_TO_FORMAT = {
+    ".json": "json",
+    ".yml": "yaml",
+    ".yaml": "yaml",
+}
+
+
 def _validate_path_format(path: Path, target: Union[str, None] = None) -> str:
-    format = path.suffix[1:]  # "foo/bar.json" -> "json"
-    if format not in _VALID_FORMATS:
+    try:
+        format = _SUFFIX_TO_FORMAT[path.suffix]
+    except KeyError:
         raise ValueError
-    elif isinstance(target, str):
+
+    if isinstance(target, str):
         if target not in _VALID_FORMATS or target != format:
             raise ValueError
     elif target is not None:
